bf5ec060841a6bdb17ca677128bbbe586cf532e6
madechun <45516694@qq.com>
2019-10-18 21:21:11 +0800

    update

--- webpack/webpack.config.build.js
+++ webpack/webpack.config.build.js
@@ -123,18 +123,8 @@ module.exports = {
 	},
 
 	externals: {
-		react: {
-			root: 'React',
-			commonjs2: 'react',
-			commonjs: 'react',
-			amd: 'react'
-		},
-		'react-dom': {
-			root: 'ReactDOM',
-			commonjs2: 'react-dom',
-			commonjs: 'react-dom',
-			amd: 'react-dom'
-		}
+		react: 'React',
+		'react-dom': 'ReactDOM'
 	},
 
 	plugins: [new CleanWebpackPlugin()]
